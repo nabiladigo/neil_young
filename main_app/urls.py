1d4fc72b4e04ca611728ce8830cd514c7b35fdfa
Eric Fithian <ef2413@columbia.edu>
2022-02-04 13:42:47 -0500

    Updated with the full URLs and views

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), # <- here we have added the new path
-    path('about/', views.About.as_view(), name="about")
+    path('about/', views.About.as_view(), name="about"),
+    path('artists/', views.ArtistList.as_view(), name="artists")
 ]
