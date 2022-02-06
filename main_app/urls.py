141a0420eb310dd6bf1288d929a508cf8be515c7
nabiladigo <nabiladigo91@gmail.com>
2022-02-06 11:34:31 -0500

    create files and routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,14 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), # <- here we have added the new path
     path('about/', views.About.as_view(), name="about"),
-    path('artists/', views.ArtistList.as_view(), name="artists")
+    path('artists/', views.ArtistList.as_view(), name="artist_list"),
+    path('artists/new/',  views.ArtistCreate.as_view(), name="artist_create"),
+     # Our new Route including the pk param
+    path('artists/<int:pk>/', views.ArtistDetail.as_view(), name="artist_detail"),
+    # Our new Route including the pk param
+    path('artists/<int:pk>/update',views.ArtistUpdate.as_view(), name="artist_update"),
 ]
